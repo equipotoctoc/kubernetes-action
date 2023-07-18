adb5e88d1338b76008a01e6eff10943cf282202a
equipotoctoc <82477001+equipotoctoc@users.noreply.github.com>
2023-07-18 17:19:05 -0400

    Update entrypoint.sh
    by gerard

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
+echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result="$(kubectl $1)"
-
+result=$(kubectl $1)
 status=$?
-echo ::set-output name=result::$result
+
+echo "::set-output name=result::$result"
 echo "$result"
-if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
+
+if [ $status -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
