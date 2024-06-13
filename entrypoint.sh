12138c53942ac1802bd5c25a1dd69ffcdc27d5a1
Gerardo Manuel Muñoz Illesca <5135378+GMIllesca@users.noreply.github.com>
2024-06-13 12:14:52 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,49 @@
 echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-result=$(kubectl $1)
-status=$?
+# Ejecutar el comando de kubectl pasado como argumento
+kubectl $@
 
-echo "::set-output name=result::$result"
-echo "$result"
-
-if [ $status -eq 0 ]; then
-  exit 0
-else
+# Verificar si el comando anterior fue exitoso
+if [ $? -ne 0 ]; then
+  echo "kubectl command failed"
   exit 1
 fi
+
+# Verificar si la variable PROJECT está definida
+if [ -z "$PROJECT" ]; then
+  echo "PROJECT variable is not set. Skipping deployment validation."
+  exit 0
+fi
+
+# Verificar si la variable PROJECT está definida
+if [ -z "$NAMESPACE" ]; then
+  echo "NAMESPACE variable is not set. Skipping deployment validation."
+  exit 0
+fi
+
+# Aquí comienza la lógica de espera
+ATTEMPTS=0
+MAX_ATTEMPTS=20
+SLEEP_TIME=30
+READY_REPLICAS=0
+DESIRED_REPLICAS=$(kubectl get deployment $PROJECT -n default -o jsonpath='{.spec.replicas}')
+
+until [ $READY_REPLICAS -eq $DESIRED_REPLICAS ] || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
+  echo "Waiting for all pods to be ready..."
+  READY_REPLICAS=$(kubectl get deployment $PROJECT -n $NAMESPACE -o jsonpath='{.status.readyReplicas}')
+  if [ -z "$READY_REPLICAS" ]; then
+    READY_REPLICAS=0
+  fi
+  echo "Ready replicas: $READY_REPLICAS"
+  ATTEMPTS=$((ATTEMPTS + 1))
+  sleep $SLEEP_TIME
+done
+
+if [ $READY_REPLICAS -ne $DESIRED_REPLICAS ]; then
+  echo "Deployment did not complete successfully"
+  exit 1
+fi
+
+echo "Deployment completed successfully"
+exit 0
